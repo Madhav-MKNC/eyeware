40cc85b7be850d2453f465128f1d1e1f4c7f5034
unknown <madhavampire@gmail.com>
2023-05-13 21:01:47 +0530

    warspeed

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-# web app
+import os
 
 from flask import Flask
 from flask import render_template, redirect, request, url_for, send_file
+from werkzeug.utils import secure_filename
+
 from PIL import Image
+from image_process import process_image
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 # start web app
 def start(host="localhost", port=80, debug=True):
@@ -16,18 +20,25 @@ def start(host="localhost", port=80, debug=True):
     except Exception as err:
         print("[error]", str(err))
 
+# routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(r'index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    # Get the uploaded image file
-    img_file = request.files['image']
-
+    file = request.files['file']
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        print('[*] File uploaded successfully')
+    else:
+        print("[!] No file uploaded")
+        return 'No file uploaded'
+    
     # Process the image
-    img = Image.open(img_file)
-    processed_img = 1# Your image processing code here
+    img = Image.open(file)
+    processed_img = process_image(img) # Your image processing code here
 
     # Save the processed image to a temporary file
     temp_file = 'temp.png'
@@ -39,6 +50,8 @@ def upload():
 
 
 
+
+# main
 if __name__ == "__main__":
     # run on defaults (host:port)
-    app.run(debug=True)
+    start(debug=True)
