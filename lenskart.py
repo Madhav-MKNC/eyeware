44497a359be615ccffc1e7bfbce72954eb2232b6
unknown <madhavampire@gmail.com>
2023-05-13 15:38:40 +0530

    eyeware src

---
+++ lenskart.py
@@ -0,0 +1,23 @@
+# fetching data from lenskart
+
+from modules import *
+
+lenskart_url = "https://www.lenskart.com/eyeglasses.html"
+
+def fetch_data(url, content_class, save_to_file = "eyewares-from-lenskart.txt"):
+    print(f"[+] fetching {url}...")
+
+    # page = requests.get(url)
+    with open('page.html', encoding='utf-8') as file:
+        page = file.read()
+
+    tags = BeautifulSoup(page, 'html.parser').find_all(class_ = content_class)
+    eyewares = [data['src'] for data in tags if 'src' in data.attrs]
+
+    # saving data
+    with open(save_to_file, 'w', encoding='utf-8') as file:
+        file.write("\n".join(eyewares))
+
+fetch_data(lenskart_url, content_class = "getGaData sliderImg")
+
+    
